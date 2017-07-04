ed5de5d499effe5c392270524cdde7dde21e15a3
Unayung Chen <unayung@gmail.com>
2017-07-04 17:51:51 +0800

    easier to read

--- lesson_1/welcome.rb
+++ lesson_1/welcome.rb
@@ -1,7 +1,9 @@
 name = ARGV[0]
 name ||= ""
-msg = "#{name} Welcome to X-Program"
-msg = "#{msg} #{Time.now}"
-puts "="*msg.size
+
+msg = name + " Welcome to X-Program"
+msg = msg + " " + Time.now.to_s
+
+puts "="* msg.size
 puts msg
-puts "="*msg.size
+puts "="* msg.size
