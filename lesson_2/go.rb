d474f3f08c1aced75a2e553dac1d933264572d04
Unayung Chen <unayung@gmail.com>
2017-07-05 17:11:35 +0800

    automation by selenium

--- lesson_2/go.rb
+++ lesson_2/go.rb
@@ -12,4 +12,26 @@ visit(url)
 
 find('input#keyword').set('nintendo switch')
 find('input#doSearch').click
-sleep(30)
+
+sleep(3)
+result_size = find('span#SearchInfo span').text.to_i
+puts "result size #{result_size}"
+
+i = 0
+while i < result_size do
+  item = all('dl.col3f')[i]
+  name =  item.find('h5').text
+  price = item.find('dd.c3f ul:first-child li span span').text
+  puts name + " - " + price
+  i+=1
+  # all('dl.col3f').each do |item|
+  #   i+=1
+  #   puts i
+  #   name =  item.find('h5').text
+  #   price = item.find('dd.c3f ul:first-child li span span').text
+  #   puts name + " - " + price
+  # end
+  height = i*190
+  page.execute_script("window.scrollTo(0,#{height})")
+end
+
