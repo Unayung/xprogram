c633b3a2e60f5871ff486c56bca599cf9819b022
Unayung Chen <unayung@gmail.com>
2017-07-06 13:53:21 +0800

    lesson 1 update

--- lesson_2/go.rb
+++ lesson_2/go.rb
@@ -1,30 +1,11 @@
-require 'capybara'
-require 'capybara/dsl'
-require 'selenium-webdriver'
-
-include Capybara::DSL
-
-url = "http://24h.pchome.com.tw/"
-Capybara.javascript_driver = :selenium
-Capybara.current_driver = :selenium
-
-visit(url)
-
-find('input#keyword').set('nintendo switch')
-find('input#doSearch').click
-
-sleep(3)
-result_size = find('span#SearchInfo span').text.to_i
-puts "result size #{result_size}"
-
-i = 0
-while i < result_size do
-  item = all('dl.col3f')[i]
-  name =  item.find('h5').text
-  price = item.find('dd.c3f ul:first-child li span span').text
-  puts name + " - " + price
-  i+=1
-  height = i*190
-  page.execute_script("window.scrollTo(0,#{height})")
-end
+# 第二次上課我們要教的是自動化處理，使用的套件有
+#
+# Capybara https://github.com/teamcapybara/capybara
+#   gem install capybara
+#
+# Capybara DSL
+#   安裝 capybara 時會一併安裝
+#
+# Selenium-Webdriver https://github.com/SeleniumHQ/selenium/wiki/Ruby-Bindings
+#   gem install selenium-webdriver
 
