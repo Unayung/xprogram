6e9037863883aa9d892d5e871435c197695152f9
Unayung Chen <unayung@gmail.com>
2017-07-06 11:50:17 +0800

    remove commented

--- lesson_2/go.rb
+++ lesson_2/go.rb
@@ -24,13 +24,6 @@ while i < result_size do
   price = item.find('dd.c3f ul:first-child li span span').text
   puts name + " - " + price
   i+=1
-  # all('dl.col3f').each do |item|
-  #   i+=1
-  #   puts i
-  #   name =  item.find('h5').text
-  #   price = item.find('dd.c3f ul:first-child li span span').text
-  #   puts name + " - " + price
-  # end
   height = i*190
   page.execute_script("window.scrollTo(0,#{height})")
 end
